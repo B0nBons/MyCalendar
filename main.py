472aebdf350166ec76892b9b9c88d83ea76ef729
Baani Sandhu <165845602+B0nBons@users.noreply.github.com>
2025-11-12 14:32:57 -0700

    Add root as arg
    Updated Controller init  to include root.

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from view import View
 from controller import Controller
 # Import libraries and classes from other files
 # Check for logic issues please future me
-# For the love of everything please use pygaem
+# For the love of everything please use pygame
 
 def main():
     root = tk.Tk()
     model = Model()
     view = View(root)
-    controller = Controller(model, view)
+    controller = Controller(model, view, root)
     root.mainloop()
 
 if __name__ == "__main__":
